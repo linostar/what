584ab63091f779f9fdec83d7448f2acad9ebed91
linostar <linux.anas@gmail.com>
2015-10-09 22:08:56 +0300

    Added the base of views

--- views.py
+++ views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout, get_user
 
-# Create your views here.
+from Interactive.what.models import Teacher, Student, Quiz, Question, Answer, Result
+
+def index(request):
+	if not request.user.is_authenticated():
+		return redirect("login")
+	else:
+		return render(request, "what/index.html", {
+			'userid': get_user(request).id,
+			'username': get_user(request).username,
+			})
