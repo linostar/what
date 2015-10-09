f6789a2377b0e1cb56f5be3c699ed7399d7c9c4f
linostar <linux.anas@gmail.com>
2015-10-09 15:26:35 +0300

    Added models to models.py

--- models.py
+++ models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Teacher(User):
+	number_of_quizzes = models.IntegerField(default=0)
+
+
+class Quiz(models.Model):
+	teacher = models.ForeignKey(Teacher)
+	quiz_name = models.CharField(max_length=128)
+	created = models.DateTimeField()
+	enabled = models.BooleanField(default=True)
+	number_of_questions = models.IntegerField(default=0)
+
+
+class Question(models.Model):
+	quiz = models.ForeignKey(Quiz)
+	question_text = models.CharField(max_length=512)
+	# question_type: 0 for written answer, 1 for m-choice, 2 for m-choice m-answer
+	question_type = models.IntegerField(default=1)
+
+
+class Answer(models.Model):
+	question = models.ForeignKey(Question)
+	answer_text = models.CharField(max_length=512)
+	answer_is_correct = models.BooleanField()
+
+
+class Student(models.Model):
+	student_name = models.CharField(max_length=64)
+	number_of_quizzes = models.IntegerField(default=0)
+
+
+class Result(models.Model):
+	student = models.ForeignKey(Student)
+	quiz = models.ForeignKey(Quiz)
+	secret_code = models.CharField(max_length=16)
+	score = models.IntegerField(default=0)
