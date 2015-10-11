115d5e106e17b99f7af3c6d67c5ec2c0e22b9569
linostar <linux.anas@gmail.com>
2015-10-11 22:06:31 +0300

    Automate some fields upon model creation/deletion in admin.py

--- admin.py
+++ admin.py
@@ -11,17 +11,48 @@ class AnnalAdmin(admin.ModelAdmin):
 		self.exclude = ["created_on", "number_of_questions"]
 		return super().get_form(request, obj, **kwargs)
 
+	def save_model(self, request, obj, form, change):
+		obj.teacher.number_of_annals += 1
+		obj.teacher.save()
+		obj.save()
+
+	def delete_model(self, request, obj):
+		obj.teacher.number_of_annals -= 1
+		obj.teacher.save()
+		obj.delete()
+
 
 class QuestionAdmin(admin.ModelAdmin):
-	list_display = ["question_text", "annal"]
+	list_display = ["get_question", "annal"]
+
+	def get_question(self, obj):
+		return obj.question_text
+
+	def save_model(self, request, obj, form, change):
+		obj.annal.number_of_questions += 1
+		obj.annal.save()
+		obj.save()
+
+	def delete_model(self, request, obj):
+		obj.annal.number_of_questions -= 1
+		obj.annal.save()
+		obj.delete()
+
+	get_question.short_description = "Question"
+	get_question.admin_order_field = "question_text"
 
 
 class AnswerAdmin(admin.ModelAdmin):
-	list_display = ["answer_text", "question", "get_annal"]
+	list_display = ["get_answer", "question", "get_annal"]
+
+	def get_answer(self, obj):
+		return obj.answer_text
 
 	def get_annal(self, obj):
 		return obj.question.annal
 
+	get_answer.short_description = "Answer"
+	get_answer.admin_order_field = "answer_text"
 	get_annal.short_description = "Annal"
 	get_annal.admin_order_field = "question__annal"
 
@@ -55,8 +86,18 @@ class QuizAdmin(admin.ModelAdmin):
 		return obj.student.student_name
 
 	def get_form(self, request, obj=None, **kwargs):
-		self.exclude = ["quiz_code"]
+		self.exclude = ["quiz_code", "score"]
 		return super().get_form(request, obj, **kwargs)
+
+	def save_model(self, request, obj, form, change):
+		obj.student.number_of_quizzes += 1
+		obj.student.save()
+		obj.save()
+
+	def delete_model(self, request, obj):
+		obj.student.number_of_quizzes -= 1
+		obj.student.save()
+		obj.delete()
 
 	get_student.short_description = "Quiz for"
 	get_student.admin_order_field = "student__student_name"
